d2c7715721501240c72575e0042eb198d4c3d243
segfault0x61 <torresj1997@outlook.com>
2022-03-03 07:04:32 -0600

    Show and Hide Movies Based on Index (Slider)

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { IMovie } from '../../models/movie';
 
@@ -5,11 +12,24 @@ import { IMovie } from '../../models/movie';
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('slideFade', [
+      state('void', style({ opacity: 0 })),
+      transition('void <=> *', [animate('1s')]),
+    ]),
+  ],
 })
 export class SliderComponent implements OnInit {
   @Input() items: IMovie[] = [];
 
+  currentSlideIndex: number = 0;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentSlideIndex =
+        ++this.currentSlideIndex % (this.items.length / 2);
+    }, 10000);
+  }
 }
