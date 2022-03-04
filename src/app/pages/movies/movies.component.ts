2359d29a18f121f30bb69f36b0bbb7325eeb040d
segfault0x61 <torresj1997@outlook.com>
2022-03-03 19:18:59 -0600

    Add Movie Page Layout

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { IMovie } from '../../models/movie';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
+  movies: IMovie[] = [];
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
+    this.moviesService.getMovies('popular', 20).subscribe((movies) => {
+      this.movies = movies;
+    });
   }
-
 }
